88f78ffbe025011ae8cc45949b6ef95ead3c6108
MassonSW <glowing.darkness46@gmail.com>
2023-10-24 18:45:45 +0300

    18. end

--- scenes/sword_ability/sword_ability_controller.gd
+++ scenes/sword_ability/sword_ability_controller.gd
@@ -1,11 +1,15 @@
 extends Node
 
 @export var sword_ability: PackedScene
+
 var damage = 5
+var base_waite_time
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	base_waite_time = $Timer.wait_time
 	$Timer.timeout.connect(on_timer_timeout)
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 func on_timer_timeout():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
@@ -22,4 +26,21 @@ func on_timer_timeout():
 		
 	else:
 		sword_instance.global_position = player.global_position
+	
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if upgrade.id != "sword_rate":
+		return
+	
+	var percent_reduction = current_upgrades["sword_rate"]["quantity"] * .1
+	$Timer.wait_time = base_waite_time * (1 - percent_reduction)
+	$Timer.start()
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
