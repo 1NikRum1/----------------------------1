cc151f7080c77b447fb1926b070ef5d1b1da4691
Nikita <nek.55rumm@gmail.com>
2023-10-24 18:07:47 +0300

    начал 17 урок

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -3,7 +3,12 @@ extends PanelContainer
 @onready var name_label: Label = $%NameLabel
 @onready var description_label: Label = $%DescriptionLabel 
 
+func _ready() -> void:
+	gui_input.connect(on_gui_input) 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
+
+func on_gui_input(event: InputEvent):
+	pass
